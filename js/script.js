cde9ff45b451f51b0594e07e480542fa5d60ac92
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-08-22 14:53:07 +0200

    js progress

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 // Fase di preparazione
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
+const images = document.querySelectorAll("#carousel img");
+
+// Rendo visibile la prima img
+let currentActiveIndex = 0;
+images[currentActiveIndex].classList.add("active");
+
+// Eventi dinamici
+// Next button
+nextButton.addEventListener("click", function () {});
+
+// Prev button
+prevButton.addEventListener("click", function () {});
